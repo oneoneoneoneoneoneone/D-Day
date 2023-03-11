ed88db27fdcb260810d406e9b159f983babe06ac
oneoneoneoneoneoneone <oneoneoneoneoneoneone>
2023-03-11 17:33:23 +0900

    Edit 화면 View -> UITableViewController 변경 EditCellDelegate 추가

--- D-Day/Presentation/Edit/EditPresenter.swift
+++ D-Day/Presentation/Edit/EditPresenter.swift
@@ -13,16 +13,16 @@ import RealmSwift
 protocol EditDelegate{
     func selectItem(_ id: ObjectId)
 }
+protocol EditCellDelegate{
+    func valueChanged(_ cell: EditViewController.CellList, didChangeValue value: Any?)
+    func valueChanged(_ cell: EditViewController.CellList, didChangeValue value: UIColor?)
+}
 
 protocol EditProtocol{
-    func setAttribute()
     func setNavigation()
     func setLayout()
-    func setData(_ item: Item)
-    func setImage(_ image: UIImage!)
     
     func showCloseAlertController()
-    func showImageSelectAlertController()
     func showToast(message: String)
     func dismiss()
 }
@@ -34,66 +34,56 @@ final class EditPresenter: NSObject{
     private let repository: Repository
     private let userNotificationCenter = UNUserNotificationCenter.current()
     
-    let textViewPlaceHolder = "메모를 입력해주세요."
-    private let cellList = EditViewController2.CellList.allCases
+    final let textViewPlaceHolder = "메모를 입력해주세요."
+    private final let cellList = EditViewController.CellList.allCases
     
     private let item: Item
     
-    init(viewController: EditProtocol, delegate: EditDelegate, userDefaultsManager: UserDefaultsManager = UserDefaultsManager(), repository: Repository = Repository(), item: Item) {
+    private let editItem = Item()
+    private var image: UIImage
+
+    init(viewController: EditProtocol, delegate: EditDelegate, userDefaultsManager: UserDefaultsManager = UserDefaultsManager(), repository: Repository = Repository(), item: Item, image: UIImage = UIImage(systemName: "photo")!) {
         self.viewController = viewController
         self.delegate = delegate
         self.userDefaultsManager = userDefaultsManager
         self.repository = repository
         self.item = item
+        self.image = image
     }
     
     func viewDidLoad(){
         viewController.setNavigation()
         viewController.setLayout()
-        viewController.setAttribute()
-        
-        viewController.setData(item)
-        
-        guard let image = Repository().loadImageFromDocumentDirectory(imageName: item.id.stringValue) else {
-            viewController.setImage(.init(systemName: "photo"))
-            return
-        }
-        viewController.setImage(image)
     }
     
     func leftCancelButtonTap(){
         viewController.showCloseAlertController()
     }
     
-    func rightSaveButtonTap(title: String!, titleColor: UIColor!, date: Date!, isBgColor: Bool!, bgColor: UIColor!, isBgImage: Bool!, bgImage: UIImage!, isCircle: Bool!, memo: String!){
-        
-        guard let title = title, title != "" else {
+    func rightSaveButtonTap(){
+        if editItem.title == "" {
             viewController.showToast(message: "제목을 입력해주세요.")
             return
         }
-        
-        //        if isBgColor && bgColor == nil{
-        //            viewController.showToast(message: "배경색을 선택해주세요.")
-        //            return
-        //        }
-        if isBgImage && bgImage == .init(systemName: "photo"){
+        if editItem.isBackgroundImage && image == UIImage(systemName: "photo"){
             viewController.showToast(message: "배경이미지를 선택해주세요.")
             return
         }
-        if bgImage != .init(systemName: "photo"){
-            repository.saveImageToDocumentDirectory(imageName: item.id.stringValue, image: bgImage)
+        
+        if image != UIImage(systemName: "photo"){
+            repository.saveImageToDocumentDirectory(imageName: item.id.stringValue, image: image)
         }
         
         let saveItem = Item()
         saveItem.id = item.id
-        saveItem.title = title
-        saveItem.titleColor = titleColor?.rgbString ?? "FF000000"
-        saveItem.date = date
-        saveItem.isBackgroundColor = isBgColor
-        saveItem.backgroundColor = bgColor?.rgbString ?? "FFFFFFFF"
-        saveItem.isBackgroundImage = isBgImage
-        saveItem.isCircle = isCircle
-        saveItem.memo = memo == textViewPlaceHolder ? "" : memo
+        saveItem.title = editItem.title
+        saveItem.titleColor = editItem.titleColor
+        saveItem.date = editItem.date
+        saveItem.isBackgroundColor = editItem.isBackgroundColor
+        saveItem.backgroundColor = editItem.backgroundColor
+        saveItem.isBackgroundImage = editItem.isBackgroundImage
+        saveItem.isCircle = editItem.isCircle
+        saveItem.memo = editItem.memo == textViewPlaceHolder ? "" : editItem.memo
         
         //저장
         repository.editItem(saveItem)
@@ -107,126 +97,135 @@ final class EditPresenter: NSObject{
         viewController.dismiss()
         delegate.selectItem(item.id)
     }
-    
-    func bgImageButtonTap(){
-        viewController.showImageSelectAlertController()
-    }
-    
-//    func isSwitchChangedValue(){
-//        viewController.
-//    }
 }
 
-//extension EditPresenter: DetailDelegate{
-//    func setData(item: Item){
-//        self.item = item
-//    }
-//}
 
-//
-//extension EditPresenter: UITableViewDelegate{
-//}
-//
-//extension EditPresenter: UITableViewDataSource{
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        cellList.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        cellList.filter{ item in
-//            item.section.rawValue == section
-//        }.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        let row = indexPath.row + cellList.filter{ item in
-//            item.section.rawValue < indexPath.section
-//        }.count
-//
-//        if cellList[row] == .date{
-//            return 240
-//        }
-//        else if cellList[row] == .backgroundImage{
-//            return 120
-//        }
-//        else if cellList[row] == .memo{
-//            return 120
-//        }
-//        else{
-//            return 60
-//        }
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let row = indexPath.row + cellList.filter{ cell in
-//            cell.section.rawValue < indexPath.section
-//        }.count
-//
-//        switch cellList[row] {
-//        case .title:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewTitleCell", for: indexPath) as! EditTableViewTitleCell
-//            cell.selectionStyle = .none
-//            cell.setPlaceholderText(text: cellList[row].text)
-//            cell.bind(viewModel.title, viewModel.titleColor)
-//            cell.setDate(text: item.title, color: item.titleColor)
-//
-//            return cell
-//        case .date:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewDateCell", for: indexPath) as! EditTableViewDateCell
-//            cell.selectionStyle = .none
-//            cell.setLabelText(text: cellList[row].text)
-//            cell.bind(viewModel.date)
-//            cell.setDate(date: item.date)
-//
-//            return cell
-////        case .isStartCount:
-////            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewToggleCell", for: indexPath) as! EditTableViewToggleCell
-////            cell.selectionStyle = .none
-////            cell.setLabelText(text: cellList[row].text)
-////            cell.bind(viewModel.isStartCount)
-////            cell.setDate(isOn: item.isStartCount)
-////
-////            return cell
-////        case .repeatCode:
-////            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewSegmentCell", for: indexPath) as! EditTableViewSegmentCell
-////            cell.selectionStyle = .none
-////            cell.setLabelText(text: cellList[row].text)
-////            cell.bind(viewModel.repeatCode)
-////            cell.setDate(value: item.repeatCode)
-////
-////            return cell
-//        case .backgroundColor:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewColorCell", for: indexPath) as! EditTableViewColorCell
-//            cell.selectionStyle = .none
-//            cell.setLabelText(text: cellList[row].text)
-//            cell.bind(viewModel.isBgColor, viewModel.bgColor)
-//            cell.setDate(isOn: item.isBackgroundColor, color: item.backgroundColor)
-//
-//            return cell
-//        case .backgroundImage:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewImageCell", for: indexPath) as! EditTableViewImageCell
-//            cell.selectionStyle = .none
-//            cell.setLabelText(text: cellList[row].text)
-//            cell.bind(viewModel.isBgImage)
-//            cell.setDate(isOn: item.isBackgroundImage, id: item.id.stringValue)
-//
-//            return cell
-//        case .isCircle:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewToggleCell", for: indexPath) as! EditTableViewToggleCell
-//            cell.selectionStyle = .none
-//            cell.setLabelText(text: cellList[row].text)
-//            delegate.bind(item.isCircle)
-//            delegate.setDate(item.isCircle)
-//
-//            return cell
-//        case .memo:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewMenoCell", for: indexPath) as! EditTableViewMemoCell
-//            cell.selectionStyle = .none
-////            cell.setPlaceholderText(text: cellList[row].text)
-//            cell.bind(viewModel.memo)
-//            cell.setDate(text: item.memo)
-//
-//            return cell
-//        }
-//    }
-//}
+extension EditPresenter: EditCellDelegate{
+    
+    func valueChanged(_ cell: EditViewController.CellList, didChangeValue value: Any?) {
+        switch cell{
+        case .title:
+            if value is String{
+                editItem.title = value as! String
+            }
+        case .date:
+            if value is Date{
+                editItem.date = value as! Date
+            }
+        case .backgroundColor:
+            if value is Bool{
+                editItem.isBackgroundColor = value as! Bool
+            }
+        case .backgroundImage:
+            if value is UIImage{
+                image = value as! UIImage
+            }
+            if value is Bool{
+                editItem.isBackgroundImage = value as! Bool
+            }
+        case .isCircle:
+            if value is Bool{
+                editItem.isCircle = value as! Bool
+            }
+        case .memo:
+            if value is String{
+                editItem.memo = value as! String
+            }
+        }
+    }
+    
+    func valueChanged(_ cell: EditViewController.CellList, didChangeValue value: UIColor?) {
+        switch cell{
+        case .title:
+            editItem.titleColor = value?.rgbString ?? "FF000000"
+        case .backgroundColor:
+            editItem.backgroundColor = value?.rgbString ?? "FFFFFFFF"
+        default: break
+        }
+    }
+}
+
+extension EditPresenter: UITableViewDelegate{
+}
+
+extension EditPresenter: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        cellList.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cellList.filter{ item in
+            item.section.rawValue == section
+        }.count
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let row = indexPath.row + cellList.filter{ item in
+            item.section.rawValue < indexPath.section
+        }.count
+
+        if cellList[row] == .date{
+            return 240
+        }
+        else if cellList[row] == .backgroundImage{
+            return 120
+        }
+        else if cellList[row] == .memo{
+            return 120
+        }
+        else{
+            return 60
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        				let row = indexPath.row + cellList.filter{ cell in
+            cell.section.rawValue < indexPath.section
+        }.count
+
+        switch cellList[row] {
+        case .title:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewTitleCell", for: indexPath) as! EditTableViewTitleCell
+            cell.selectionStyle = .none
+            cell.bind(delegate: self, cell: cellList[row])
+            cell.setDate(text: item.title, color: item.titleColor)
+
+            return cell
+        case .date:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewDateCell", for: indexPath) as! EditTableViewDateCell
+            cell.selectionStyle = .none
+            cell.bind(delegate: self, cell: cellList[row])
+            cell.setDate(date: item.date)
+
+            return cell
+        case .backgroundColor:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewColorCell", for: indexPath) as! EditTableViewColorCell
+            cell.selectionStyle = .none
+            cell.bind(delegate: self, cell: cellList[row])
+            cell.setDate(isOn: item.isBackgroundColor, color: item.backgroundColor)
+
+            return cell
+        case .backgroundImage:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewImageCell", for: indexPath) as! EditTableViewImageCell
+            cell.selectionStyle = .none
+            cell.bind(delegate: self, cell: cellList[row])
+            cell.setDate(isOn: item.isBackgroundImage, id: item.id.stringValue)
+
+            return cell
+        case .isCircle:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewToggleCell", for: indexPath) as! EditTableViewToggleCell
+            cell.selectionStyle = .none
+            cell.bind(delegate: self, cell: cellList[row])
+            cell.setDate(isOn: item.isCircle)
+
+            return cell
+        case .memo:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewMenoCell", for: indexPath) as! EditTableViewMemoCell
+            cell.selectionStyle = .none
+            cell.bind(delegate: self, cell: cellList[row])
+            cell.setDate(text: item.memo)
+
+            return cell
+        }
+    }
+}
--- D-Day/Presentation/Edit/EditViewController.swift
+++ D-Day/Presentation/Edit/EditViewController.swift
@@ -6,47 +6,14 @@
 //
 
 import UIKit
-import RxSwift
 
 class EditViewController: UIViewController{
-    let disposeBag = DisposeBag()
-    final let cellList = CellList.allCases
-    //var item = Item()
-//    let cellList = [["제목", "날짜", "시작일", "반복"], ["배경색", "배경이미지", "동그랗게"], ["메모"]]
-//    final let dataList = ["제목", "날짜", "시작일", "반복"]
-//    final let viewList = ["배경색", "배경이미지", "동그랗게"]
-//    final let extList = ["메모"]
-    var viewModel = EditViewModel()
-    var item = Item()
-    
-    let rightSaveButton = UIBarButtonItem()
-    let tableView = UITableView(frame: .zero, style: .insetGrouped)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-                
-        setAttribute()
-        setNavigation()
-        setLayout()
-    }
-    
-    private func setNavigation(){
-//        navigationController?.navigationBar.isTranslucent = true
-        navigationItem.rightBarButtonItem = rightSaveButton
-    }
-    
-    private func setAttribute(){
-        rightSaveButton.title = "저장"
-//        rightSaveButton.rx.tap
-//            .subscribe{_ in
-//                self.rightSaveButtonTap()
-//            }
-//            .disposed(by: disposeBag)
-                
-        tableView.delegate = self
-        tableView.dataSource = self
-        
+    private lazy var presenter = EditPresenter(viewController: self, delegate: delegate, item: item)
+    private let delegate: EditDelegate
+    private var item = Item()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.register(EditTableViewTitleCell.self, forCellReuseIdentifier: "EditTableViewTitleCell")
         tableView.register(EditTableViewDateCell.self, forCellReuseIdentifier: "EditTableViewDateCell")
         tableView.register(EditTableViewToggleCell.self, forCellReuseIdentifier: "EditTableViewToggleCell")
@@ -54,172 +21,82 @@ class EditViewController: UIViewController{
         tableView.register(EditTableViewColorCell.self, forCellReuseIdentifier: "EditTableViewColorCell")
         tableView.register(EditTableViewImageCell.self, forCellReuseIdentifier: "EditTableViewImageCell")
         tableView.register(EditTableViewMemoCell.self, forCellReuseIdentifier: "EditTableViewMenoCell")
+
+        tableView.delegate = presenter
+        tableView.dataSource = presenter
+
+        return tableView
+    }()
+    
+    init(delegate: EditDelegate = MainPresenter(viewController: MainViewController()), item: Item = Item()){
+        self.delegate = delegate
+        self.item = item
+        
+        super.init(nibName: nil, bundle: nil)
     }
     
-    private func setLayout(){
-        view.addSubview(tableView)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        presenter.viewDidLoad()
+    }
+}
+
+extension EditViewController: EditProtocol{
+    func setNavigation(){
+        var leftCancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(leftCancelButtonTap))
+        var rightSaveButton = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(rightSaveButtonTap))
+
+        navigationItem.leftBarButtonItem = leftCancelButton
+        navigationItem.rightBarButtonItem = rightSaveButton
+        navigationController?.navigationBar.tintColor = .label
+        navigationController?.navigationBar.backgroundColor = .systemGray6
+    }
+
+    func setLayout(){
+        view.addSubview(tableView)
+
         tableView.snp.makeConstraints{
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.bottom.equalToSuperview()
         }
     }
     
-    func bind(_ viewModel: EditViewModel){
-        self.viewModel = viewModel
+    func showCloseAlertController() {
+        let alertController = UIAlertController(title: "작성중인 내용이 있습니다. 닫으시겠습니까?", message: nil, preferredStyle: .alert)
         
-        viewModel.saveItem
-            .emit(to: self.rx.setSave)//.map{a in self.item = a}
-            .disposed(by: disposeBag)
-            
-        viewModel.id
-            .accept(item.id)    //relay의 추가는 .accept()로 접근
+        let closeAction = UIAlertAction(title: "닫기", style: .destructive){ [weak self] _ in
+            self?.dismiss(animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
         
-        rightSaveButton.rx.tap
-            .bind(to: viewModel.saveButtonTapped)
-            .disposed(by: disposeBag)
+        [closeAction, cancelAction].forEach{action in
+            alertController.addAction(action)
+        }
+        
+        present(alertController, animated: true)
+    }
+        
+    func showToast(message: String) {
+        Util.showToast(view: self.view, message: message)
     }
     
-    func setData(item: Item){
-        self.item = item
+    func dismiss() {
+        self.dismiss(animated: true)
     }
 }
 
-extension EditViewController: UITableViewDelegate, UITableViewDataSource{
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        CellType.allCases.count
+extension EditViewController{
+    @objc func leftCancelButtonTap(){
+        presenter.leftCancelButtonTap()
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        cellList.filter{ item in
-            item.section.rawValue == section
-        }.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let row = indexPath.row + cellList.filter{ item in
-            item.section.rawValue < indexPath.section
-        }.count
-        
-        if cellList[row] == .date{
-            return 240
-        }
-        else if cellList[row] == .backgroundImage{
-            return 120
-        }
-        else if cellList[row] == .memo{
-            return 120
-        }
-        else{
-            return 60
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = indexPath.row + cellList.filter{ cell in
-            cell.section.rawValue < indexPath.section
-        }.count
-        
-        switch cellList[row] {
-        case .title:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewTitleCell", for: indexPath) as! EditTableViewTitleCell
-            cell.selectionStyle = .none
-            cell.setPlaceholderText(text: cellList[row].text)
-            cell.bind(viewModel.title, viewModel.titleColor)
-            cell.setDate(text: item.title, color: item.titleColor)
-            
-            return cell
-        case .date:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewDateCell", for: indexPath) as! EditTableViewDateCell
-            cell.selectionStyle = .none
-            cell.setLabelText(text: cellList[row].text)
-            cell.bind(viewModel.date)
-            cell.setDate(date: item.date)
-            
-            return cell
-//        case .isStartCount:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewToggleCell", for: indexPath) as! EditTableViewToggleCell
-//            cell.selectionStyle = .none
-//            cell.setLabelText(text: cellList[row].text)
-//            cell.bind(viewModel.isStartCount)
-//            cell.setDate(isOn: item.isStartCount)
-//
-//            return cell
-//        case .repeatCode:
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewSegmentCell", for: indexPath) as! EditTableViewSegmentCell
-//            cell.selectionStyle = .none
-//            cell.setLabelText(text: cellList[row].text)
-//            cell.bind(viewModel.repeatCode)
-//            cell.setDate(value: item.repeatCode)
-//
-//            return cell
-        case .backgroundColor:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewColorCell", for: indexPath) as! EditTableViewColorCell
-            cell.selectionStyle = .none
-            cell.setLabelText(text: cellList[row].text)
-            cell.bind(viewModel.isBgColor, viewModel.bgColor)
-            cell.setDate(isOn: item.isBackgroundColor, color: item.backgroundColor)
-            
-            return cell
-        case .backgroundImage:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewImageCell", for: indexPath) as! EditTableViewImageCell
-            cell.selectionStyle = .none
-            cell.setLabelText(text: cellList[row].text)
-            cell.bind(viewModel.isBgImage)
-            cell.setDate(isOn: item.isBackgroundImage, id: item.id.stringValue)
-            
-            return cell
-        case .isCircle:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewToggleCell", for: indexPath) as! EditTableViewToggleCell
-            cell.selectionStyle = .none
-            cell.setLabelText(text: cellList[row].text)
-            cell.bind(viewModel.isCircle)
-            cell.setDate(isOn: item.isCircle)
-            
-            return cell
-        case .memo:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewMenoCell", for: indexPath) as! EditTableViewMemoCell
-            cell.selectionStyle = .none
-//            cell.setPlaceholderText(text: cellList[row].text)
-            cell.bind(viewModel.memo)
-            cell.setDate(text: item.memo)
-            
-            return cell
-        }
-    }
-    
-}
-
-extension Reactive where Base: EditViewController{
-    var setSave: Binder<Item>{
-        return Binder(base) {base, data in
-            if (data.title == ""){
-                Util.showToast(view: base.view, message: "제목을 입력해주세요")
-                return
-            }
-            
-            //저장
-            Repository().edit(data: data)
-            
-            //알림시간 불러오기
-            let alertTime = Util.getAlertTime()
-            
-            //알림 추가
-            let userNotificationCenter = UNUserNotificationCenter.current()
-            userNotificationCenter.addNotificationRequest(by: data, alertTime: alertTime ?? AlertTime(isOn: true, day: 0, time: Date.now))
-                     
-            if base.navigationController?.viewControllers.filter({$0 is DetailViewController}).count == 0{
-                let detailViewController = DetailViewController()
-                detailViewController.setData(item: data)
-                base.navigationController?.pushViewController(detailViewController, animated: true)
-            }
-            else{
-                let detailViewController = base.navigationController?.viewControllers.filter{$0 is DetailViewController}.first as! DetailViewController
-                detailViewController.setData(item: data)
-                base.navigationController?.popToViewController(detailViewController, animated: true)
-            }
-        }
+    @objc func rightSaveButtonTap(){
+        presenter.rightSaveButtonTap()
     }
 }
 
@@ -229,7 +106,7 @@ extension EditViewController{
         case view = 1
         case ext = 2
     }
-    
+
     enum CellList: CaseIterable{
         case title, date, backgroundColor, backgroundImage, isCircle, memo
         // isStartCount, repeatCode
@@ -244,7 +121,7 @@ extension EditViewController{
                 return .ext
             }
         }
-        
+
         var text: String{
             switch self{
             case .title:

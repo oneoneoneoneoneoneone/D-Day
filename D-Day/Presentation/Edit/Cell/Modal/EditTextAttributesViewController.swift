415e5e934c907b78f1d08116e753386e4e6a4889
oneoneoneoneoneoneone <0ne1_1@naver.com>
2023-05-24 01:54:24 +0900

    [Feat] Edit/Cell/Modal - EditTextAttributeViewController class 생성

---
+++ D-Day/Presentation/Edit/Cell/Modal/EditTextAttributesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditTextAttributeViewController.swift
+//  D-Day
+//
+//  Created by hana on 2023/05/23.
+//
+
+import UIKit
+
+class EditTextAttributesViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
